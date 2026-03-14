ffcd072cd76d7317973cba1a4866be7f95eb9167
91830 <91830@POULINS>
2026-03-14 18:04:01 +0530

    Excel read initial commit

---
+++ ExcelReadMaven/src/main/java/excelread/ExcelReadMain.java
@@ -0,0 +1,13 @@
+package excelread;
+
+import java.io.IOException;
+
+public class ExcelReadMain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+System.out.println(ExcelRead1.getStringData(0, 0));
+//System.out.println(ExcelRead1.getIntegerData(1, 0));
+	}
+
+}
